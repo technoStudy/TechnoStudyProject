a056e72d5d4ea8a578efb66ab980e9fe94df2459
lanakozlov <svetlana39@hotmail.com>
2021-11-22 20:10:09 -0500

    I created a new class

---
+++ src/LanaNewPackage/NewClass1.java
@@ -0,0 +1,12 @@
+package LanaNewPackage;
+
+import java.util.logging.SocketHandler;
+
+public class NewClass1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Lana Kozlov");
+        System.out.println("Don't give up");
+        System.out.println("You can concur Java");
+    }
+}
