c5ba0e5658732ca7f314b6803c4441581dc8fd3c
amsal_memic <amsalmemic90@gmail.com>
2021-11-22 21:30:57 -0500

    Sorry for late respond

--- src/AmsalPackage/newTest.java
+++ src/AmsalPackage/newTest.java
@@ -1,7 +1,9 @@
 package AmsalPackage;
 
 public class newTest {
+
     public static void main(String[] args) {
-        System.out.println("Test");
+
+        System.out.println("Test one two three");
     }
 }
