62b126aac2d2c4b0e4d97463862973d2d9c91f29
AdemDeniz <93459625+adembej@users.noreply.github.com>
2021-11-22 21:38:34 -0500

    newClass by Adem Deniz Garic.

--- src/AdemGaric/Main.java
+++ src/AdemGaric/Main.java
@@ -3,11 +3,6 @@ package AdemGaric;
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("Hello Halit, how are you ? ");
-        System.out.println();
-
+        System.out.println("Hello, hello");
     }
-
-
 }
